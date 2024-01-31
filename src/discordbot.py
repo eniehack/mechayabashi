68c84d2257fc85c06bf4df8027d1e666a82b3560
Nakaya <eniehack@outlook.jp>
2024-01-31 20:35:46 +0900

    feat: discord bot

---
+++ src/discordbot.py
@@ -0,0 +1,29 @@
+from discord import app_commands, Intents, Client, Interaction
+from classopt import classopt, config
+from pathlib import Path
+import markovify
+
+@classopt
+class CLIArgs:
+    token: str = config(long=True, required=True)
+    dic: Path = config(long=True, required=True)
+
+args = CLIArgs.from_args()
+intents = Intents.default()
+intents.message_content = True
+client = Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+with open(args.dic, 'r') as f:
+    text_model = markovify.Text.from_json(f.read())
+
+@tree.command(name="generate", description="マルコフ連鎖で文章を生成します")
+async def generate(ctx: Interaction):
+    await ctx.response.send_message(text_model.make_sentence())
+
+@client.event
+async def on_ready():
+    print("ready")
+    await tree.sync()
+
+client.run(args.token)
